150af92cda944c3904cc9e98e364d297de7aff34
Darya Suhova <45993488+suhova@users.noreply.github.com>
2021-02-09 22:19:23 +0300

    fix algorithm

--- scheduler/AttestationType.java
+++ scheduler/AttestationType.java
@@ -4,16 +4,14 @@ public class AttestationType {
     public String type;
     public int pauseBefore;
     public int pauseAfter;
-    public int maxInDay;
     public int duration;
     public int countPerDay;
 
-    public AttestationType(String type, int pauseBefore, int pauseAfter, int duration, int maxInDay, int countPerDay) {
+    public AttestationType(String type, int pauseBefore, int pauseAfter, int duration, int countPerDay) {
         this.type = type;
         this.pauseBefore = pauseBefore;
         this.pauseAfter = pauseAfter;
         this.duration = duration;
-        this.maxInDay = maxInDay;
         this.countPerDay = countPerDay;
     }
 }
